2b0b6b6ffab79b55b8bf9016d30dec3e9a9ec9db
Manu-Madhu <manum66466@gmail.com>
2024-05-20 15:29:02 +0530

    landing button update

--- src/components/home/LandingPage.jsx
+++ src/components/home/LandingPage.jsx
@@ -5,7 +5,7 @@ const LandingPage = () => {
     <div className="bg-[#F9DB79] w-full h-[600px] ">
       <div className="max-w-[1280px] mx-auto w-full h-full  flex flex-col items-center justify-center">
         <img src={logo} alt="logo" className="object w-[450px]" />
-        <button className="border-2 border-[#2F2E2E] p-3 text-sm sm:w-[200px] mt-4">OUR MENU</button>
+        <button className="border-2 border-[#2F2E2E] p-2 font-semibold px-5 text-sm sm:w-[200px] mt-4">OUR MENU</button>
       </div>
     </div>
   );
