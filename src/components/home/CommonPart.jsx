0412e6ac08afdf19572a945b1d4defe78a503198
Manu-Madhu <manum66466@gmail.com>
2024-05-20 17:13:58 +0530

    Update the Home with mobile responsive

--- src/components/home/CommonPart.jsx
+++ src/components/home/CommonPart.jsx
@@ -1,23 +1,44 @@
 const SecondPart = ({ img, imagePosition, content }) => {
   return (
-    <div className="flex flex-col sm:flex-row items-center justify-center relative w-full h-[650px] overflow-hidden">
+    <div className="flex flex-col sm:flex-row items-center justify-center relative w-full sm:h-[650px] overflow-hidden mb-10 sm:mb-0">
       <div
         className={`sm:w-1/2 h-full
-        ${ imagePosition === "left" ? "left-0 " : "right-0" } absolute bg-red-500`}
+        ${ imagePosition === "left" ? "left-0 " : "right-0" } absolute `}
       >
-        <img src={img} alt="coverImg" className="object-cover h-full" />
+        <img src={img} alt="coverImg" className="object-cover h-full hidden sm:block" />
       </div>
-      <div className="max-w-[1280px] mx-auto z-10 flex items-center justify-center">
-        <div className={`w-1/2  ${ imagePosition === "left" ? "order1 " : "order-2" }`}></div>
-        <div className="w-1/2 p-20 flex flex-col items-center justify-center ">
-          <div className={`${ imagePosition === "left" ? "text-start" : "text-end"}`}>
+      <div className="w-full h-full max-w-[1280px] mx-auto z-10 flex flex-col sm:flex-row items-center justify-center">
+        {/* Desktop view */}
+        <div className={`sm:w-1/2 ${ imagePosition === "left" ? "sm:order1 " : "sm:order-2" } hidden sm:block`}>
+          <img src={img} alt="" className="sm:hidden"/>
+        </div>
+
+        <div className="sm:w-1/2 sm:p-20 flex flex-col items-center justify-center bg-white p-3">
+          
+          {/* desktop view */}
+          <div className={`${ imagePosition === "left" ? "text-start" : "text-end"} hidden sm:block`}>
             <h1 className="font-bold text-3xl">{content?.title}</h1>
             <h5 className="font-semibold text-">{content?.subHeading}</h5>
             <p className="my-5 font-semibold">{content?.description}</p>
-            <button className="p-2.5 sm:w-[150px] border-2 font-semibold border-primaryColor mt-10">
+            <button className="p-2 sm:w-[150px] border-2 w-[200px] font-semibold border-primaryColor mt-10">
               MENU
             </button>
           </div>
+
+          {/* mobile view */}
+          <div className={`sm:hidden`}>
+            <h1 className="font-bold text-3xl">{content?.title}</h1>
+            <h5 className="font-semibold text-">{content?.subHeading}</h5>
+            <p className="my-5 font-semibold">{content?.description}</p>
+            <button className="p-2 sm:w-[150px] border-2 w-[200px] font-semibold border-primaryColor mt-10">
+              MENU
+            </button>
+          </div>
+        </div>
+        
+        {/* Mobile view */}
+        <div className={`sm:w-1/2 ${ imagePosition === "left" ? "sm:order1 " : "sm:order-2" }  sm:hidden`}>
+          <img src={img} alt="" className="sm:hidden"/>
         </div>
       </div>
     </div>
