139bbf32fc730df500176620814dcad6c5941dee
gayatrii11 <gayathrivk114@gmail.com>
2024-05-22 19:46:27 +0530

    menupage

---
+++ src/components/menu/FlavorManner.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const FlavorManner = () => {
+  return (
+    <div>
+    <div className='bg-[#afe2d1]'>
+    <div className='container h-60 text-center flex flex-col items-center justify-center text-white'>
+      <p>OUR</p>
+      <h1 className='text-7xl'>FLAVORS</h1>
+      </div>
+      </div>
+   
+    </div>
+  )
+}
+
+export default FlavorManner
+
