6ec4898e1e7a2a6eaddd05f741e14401327b0419
Manu-Madhu <manum66466@gmail.com>
2024-05-24 18:42:12 +0530

    Update tha images with aws links

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,30 +3,31 @@ import SecondPart from "../components/home/CommonPart";
 import product1 from "../assets/products/pro1.jpg";
 import product2 from "../assets/products/pro2.jpg";
 import product3 from "../assets/products/pro3.jpg";
+import product4 from "../assets/products/pro4.jpg";
 import HorizontalPart from "../components/home/HorizontalPart";
-import banner from "../assets/home/Banner1.jpg";
+// import banner from "../assets/home/Banner1.jpg";
 import banner2 from "../assets/home/Banner2.png";
-import banner3 from "../assets/home/Banner3.jpg";
-import img1 from "../assets/menu/0D8A9866.jpg";
-import Slider from "react-slick";
+// import banner3 from "../assets/home/Banner3.jpg";
+// import img1 from "../assets/menu/0D8A9866.jpg";
+// import Slider from "react-slick";
 import { CarousalData } from "../utils/Data";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 const Home = () => {
-  const settings = {
-    // dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 6,
-    slidesToScroll: 1,
-  };
+  // const settings = {
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 6,
+  //   slidesToScroll: 1,
+  // };
 
   return (
     <div>
       <LandingPage />
       <SecondPart
-        img={product1}
+        img={product4}
         imagePosition={"left"}
         content={{
           title: "OUR FLAVORS",
@@ -37,7 +38,7 @@ const Home = () => {
       />
 
       <HorizontalPart
-        img={banner}
+        img={"https://juzastorage.s3.ap-southeast-2.amazonaws.com/test/Banner1.jpg"}
         data={{
           top: "ENJOY",
           title: "DAIRY FREE",
@@ -77,7 +78,7 @@ const Home = () => {
             "I m a paragraph. Click here to add your own text and edit me. It s easy. Just click “Edit Text” or double click me to add your own content and make changes to the font.",
         }}
       />
-      <HorizontalPart img={banner3} data={{}} />
+      <HorizontalPart img={"https://juzastorage.s3.ap-southeast-2.amazonaws.com/test/Banner3.jpg"} data={{}} />
 
       <div className="flex flex-col items-center justify-center w-full overflow-hidden">
         <div className="py-10 max-w-[1280px] mx-auto ">
