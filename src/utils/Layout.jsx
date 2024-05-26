80b0799029e97f42b6ba79ea59bd9d32439149ae
Manu-Madhu <manum66466@gmail.com>
2024-05-27 01:20:52 +0530

    Add whats app option

--- src/utils/Layout.jsx
+++ src/utils/Layout.jsx
@@ -1,10 +1,11 @@
 import Nav from "../components/Nav";
 import Footer from "../components/Footer";
 import { Outlet } from "react-router-dom";
+import WhLogo from "../assets/WhLogo.png";
 
 const Layout = () => {
   return (
-    <div>
+    <div className="relative">
       <header className="max-w-full mx-auto w-full ">
         <Nav />
       </header>
@@ -14,6 +15,12 @@ const Layout = () => {
       <footer className="max-w-full mx-auto w-full ">
         <Footer />
       </footer>
+
+      <div className="fixed bottom-3 right-3 md:right-10 md:bottom-10 z-50">
+        <a href="https://wa.me/917356924029?text=hi...">
+          <img src={WhLogo} alt="whLogo" className="w-16 md:w-20" />
+        </a>
+      </div>
     </div>
   );
 };
