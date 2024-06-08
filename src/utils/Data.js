7eacd6f6e077b25137f7846c1852c129258ad80e
Manu-Madhu <manum66466@gmail.com>
2024-06-09 00:43:23 +0530

    Update the data.js

--- src/utils/Data.js
+++ src/utils/Data.js
@@ -26,74 +26,254 @@ export const CarousalData = [
     // { id: 13, img: "https://juzastorage.s3.ap-southeast-2.amazonaws.com/test/0D8A9871.jpg" },
 ]
 
-import menuImg1 from '../assets/menu/viewImages/1.png'
-import menuImg2 from '../assets/menu/viewImages/2.png'
-import menuImg3 from '../assets/menu/viewImages/3.png'
-import menuImg4 from '../assets/menu/viewImages/4.png'
-import menuImg5 from '../assets/menu/viewImages/5.png'
-import menuImg6 from '../assets/menu/viewImages/6.png'
-import menuImg7 from '../assets/menu/viewImages/7.png'
-import menuImg8 from '../assets/menu/viewImages/8.png'
-import menuImg9 from '../assets/menu/viewImages/9.png'
-import menuImg10 from '../assets/menu/viewImages/10.png'
-import menuImg11 from '../assets/menu/viewImages/Sesame chicken.png'
+import menuImg1 from  '../assets/menu/noimage/summer kidney bean salad.png'
+import menuImg2 from '../assets/menu/viewImages/Paneer Inferno .png'
+import menuImg3 from '../assets/menu/viewImages/2.png'
+import menuImg4 from '../assets/menu/viewImages/7.png'
+import menuImg5 from '../assets/menu/viewImages/6.png'
+import menuImg6 from  '../assets/menu/BUFFALO CHICKEN SALAD.jpg'
+import menuImg7 from '../assets/menu/viewImages/4.png'
+import menuImg8 from '../assets/menu/viewImages/Sesame chicken.png'
+import menuImg9 from '../assets/menu/viewImages/3.png'
+import menuImg10 from  '../assets/menu/BUFFALO CHICKEN SALAD.jpg'
+import menuImg11 from '../assets/menu/viewImages/1.png'
+import menuImg12 from '../assets/menu/viewImages/9.png'
+import menuImg13 from  '../assets/menu/shakshuka.jpg'
+import menuImg14 from  '../assets/menu/egg frittatta.jpg'
+import menuImg15 from  '../assets/menu/Chilli garlic butter Fried eggs.jpg'
+import menuImg16 from  '../assets/menu/oats with caramalized banana .jpg'
+import menuImg17 from  '../assets/menu/noimage/Oats with Apple and Peanut Butter.png'
+import menuImg18 from  '../assets/menu/OVERNIGHT OATS.jpg'
+import menuImg19 from  '../assets/menu/Coconut milk oats.jpg'
+import menuImg20 from  '../assets/menu/noimage/Paneer Rise n Rice Combo.png'
+import menuImg21 from  '../assets/menu/RICE N RISE COMBO.jpg'
+import menuImg22 from  '../assets/menu/CHICKEN AND QUINOA CURRY WITH STEAM RICE AND SAUTED VEGGIES.jpg'
+import menuImg23 from  '../assets/menu/CHICKEN AND QUINOA CURRY WITH STEAM RICE AND SAUTED VEGGIES.jpg'
+import menuImg24 from  '../assets/menu/Healthy peanut protein Smoothie.jpg'
+import menuImg25 from  '../assets/menu/noimage/Muskmelon Honey Smoothie.png'
+import menuImg26 from  '../assets/menu/Watermelon protein Smoothie.jpg'
+import menuImg27 from  '../assets/menu/Watermelon protein Smoothie.jpg'
+import menuImg28 from  '../assets/menu/noimage/Apple Beetroot Carrot Juice.png'
+import menuImg29 from  '../assets/menu/Cucumber Ginger.jpg'
+import menuImg30 from  '../assets/menu/noimage/Lime Juice with Honey & Chia Seeds.png'
+import menuImg31 from  '../assets/menu/noimage/Watermelon Juice.png'
+import menuImg32 from  '../assets/menu/noimage/Pineapple Juice.png'
+import menuImg33 from  '../assets/menu/noimage/Grape Juice.png'
+import menuImg34 from  '../assets/menu/noimage/Mosambi Juice.png'
+import menuImg35 from  '../assets/menu/noimage/Pomegranate Juice.png'
+import menuImg36 from  '../assets/menu/noimage/Tender Coconut Shake.png'
+import menuImg37 from  '../assets/menu/Raggi Hot Chocolate.jpg'
+import menuImg38 from  '../assets/menu/noimage/Fruit Bowl with Nuts.png'
+import menuImg39 from  '../assets/menu/noimage/Honey Drizzled Fruit Bowl.png'
+
 
 
 export const menuOptions = [
-    { id: 1, img: menuImg1, name: "BBQ CHICKEN", 
-    description: "something will coming soon something will coming soon", 
-    price: "1000", 
+
+    { id: 1, img: menuImg1, name: "Summer Kidney Bean Salad", 
+    description: "Flavorful meal with corn, beans, bell peppers and vegetables, tossed in a light and tangy cilantro based Greek yogurt dressing.", 
+    price: "195", 
     category: "salad" },
 
-    { id: 2, img: menuImg2, name: "Chickpea Paneer", 
-    description: "something will coming soon something will coming soon", 
-    price: "1000", 
+    { id: 2, img: menuImg2, name: "Paneer Inferno Salad Bowl", 
+    description: "Savor air fried spicy paneer, crisp romaine lettuce and a zesty coriander infused Greek yogurt dressing. Tossed with vibrant red bell peppers,  red onion, sweet corn and cherry tomato, this salad is a fresh and spicy delight for your taste buds.", 
+    price: "275", 
     category: "salad" },
 
-    { id: 3, img: menuImg3, name: "Fiery Chicken", 
-    description: "something will coming soon ", 
-    price: "1000", 
+    { id: 3, img: menuImg3, name: "Chickpea Paneer Salad", 
+    description: "Fresh mix of chickpeas, paneer, cucumber, cherry tomato, carrot, red onion and red cabbage. Tossed in a cilantro based dressing.", 
+    price: "270", 
     category: "salad" },
 
-    { id: 4, img: menuImg4, name: "Honey Chicken", 
-    description: "something will coming soon", 
-    price: "1000", 
+    { id: 4, img: menuImg4, name: "Mexican Paneer Salad", 
+    description: "Featuring spicy kidney beans, cucumber, paneer, carrot, red cabbage, fresh cilantro and zesty red bell pepper, drizzled with a tantalizing salsa dressing for that perfect kick.", 
+    price: "275", 
     category: "salad" },
 
-    { id: 5, img: menuImg5, name: "Honey Mustard Chicken Salad", 
-    description: "something will coming soon", 
-    price: "1000", 
+    { id: 5, img: menuImg5, name: "Mediterranean Quinoa Salad", 
+    description: "A fresh mediterranean inspired salad with protein packed quinoa, chickpeas, cucumber, juicy cherry tomatoes, sweet corn, red cabbage, spring onion and parsley tossed with tangy greek yogurt dressing. The perfect summer salad for a healthy and satisfying meal.", 
+    price: "270", 
     category: "salad" },
 
-    { id: 6, img: menuImg6, name: "Meditar Quinoa", 
-    description: "something will coming soon", 
-    price: "1000", 
+    { id: 6, img: menuImg6, name: "Buffalo Fiery Paneer Salad", 
+    description: "Spicy buffalo sauce marinated tender paneer, crumbled feta cheese, red cabbage, crispy carrot, red onion, juicy cherry tomatoes and refreshing cucumber, all perfectly tossed in a zesty parsley based dressing.", 
+    price: "285", 
     category: "salad" },
 
-    { id: 7, img: menuImg7, name: "Mediterranean", 
-    description: "something will coming soon", 
-    price: "1000", 
+    { id: 7, img: menuImg7, name: "Honey Chicken Salad", 
+    description: "Crispy air fried, chicken paired with 1 boiled egg, sweet cherry tomatoes, corn and refreshing romaine lettuce. Drizzled with dijon mustard and honey dressing for the perfect balance of tangy and sweet. (avocado, add-on depends on availability)", 
+    price: "299", 
     category: "salad" },
 
-    { id: 8, img: menuImg8, name: "Mexican paneer", 
-    description: "something will coming soon", 
-    price: "1000", 
+    
+    { id: 8, img: menuImg8, name: "Sesame Chicken Salad", 
+    description: "Tossed in a sesame infused dressing, this salad is a perfect combination of  crispy air fried chicken, paired with zesty orange, romaine lettuce, juicy cherry tomato, carrot and red cabbage.", 
+    price: "295", 
     category: "salad" },
 
-    { id: 9, img: menuImg9, name: "Paneer Inferno", 
-    description: "something will coming soon", 
-    price: "1000", 
+    { id: 9, img: menuImg9, name: "Fiery Chicken Salad", 
+    description: "Packed with spicy chilli sauc grilled chicken crisp veggies, juicy cherry tomatoes, bell peppers  and tossed in a cilantro based greek yogurt dressing. Deliciousness in every bite.", 
+    price: "299", 
     category: "salad" },
 
-    // { id: 10, img: menuImg10, name: "RED WINE VIN SALAD", 
-    // description: "something will coming soon", 
-    // price: "1000", 
-    // category: "salad" },
+    { id: 10, img: menuImg10, name: "Buffalo Chicken Salad", 
+    description: "Buffalo sauce marinated grilled chicken, crumbled cheese, red cabbage, crispy carrot, red onion, juicy cherry tomatoes and refreshing cucumber, all perfectly tossed in a zesty parsley based dressing.", 
+    price: "305", 
+    category: "salad" },
 
-    // { id: 11, img: menuImg11, name: "Sesame chicken", 
-    // description: "something will coming soon", 
-    // price: "1000", 
-    // category: "salad" },
 
+    { id: 11, img: menuImg11, name: "Barbeque Chicken Salad", 
+    description: "Chicken coated in smoky barbeque sauce, paired with cherry tomatoes, 1 boiled egg, red onion, vibrant red bell peppers, crispy romaine lettuce and sweet elevated with a zesty dressing featuring parsley and onion powder in greek yogurt.", 
+    price: "299", 
+    category: "salad" },
+
+    { id: 12, img: menuImg12, name: "Red Wine Vinaigrette Salad", 
+    description: "Chicken marinated in a blend of olive oil and dijon mustard, paired with crisp romaine lettuce, hard boiled eggs, cherry tomatoes, red onion, spring onion, red bell pepper and sweet corn. All tossed in a tangy red wine vinegar dressing.", 
+    price: "299", 
+    category: "salad" },
+
+    { id: 13, img: menuImg13, name: "Shakshuka", 
+    description: "Discover the Shakshuka delight—poached eggs in a spiced tomato and bell pepper stew, topped with olive oil. A flavorful journey for breakfast lovers!", 
+    price: "199", 
+    category: "allday" },
+
+    { id: 14, img: menuImg14, name: "Egg Frittata", 
+    description: "Fluffy eggs meet savory delights—vegetables, cheese, and herbs.", 
+    price: "180", 
+    category: "allday" },
+
+    { id: 15, img: menuImg15, name: "Crispy Chilly Garlic Butter Fried Eggs", 
+    description: "3 chilly butter fried eggs speckled with chilly garlic and sesame, loosely inspired by Turkish cilbir.", 
+    price: "95", 
+    category: "allday" },
+ 
+    { id: 16, img: menuImg16, name: "Oats with Caramelized Banana , Nuts and Chocolate", 
+    description: "Oats Meal with caramelized banana, almonds, raisins, and a chocolate piece. Nutritious and delicious!", 
+    price: "180", 
+    category: "allday" },
+
+    { id: 17, img: menuImg17, name: "Oats with Apple and Peanut Butter", 
+    description: "This oatmeal is perfectly sweet and balanced with a bit of savory from the nuts.", 
+    price: "190", 
+    category: "allday" },
+
+    { id: 18, img: menuImg18, name: "Nutty Harvest Oats", 
+    description: "A wholesome blend of oats, flax seeds, pumpkin seeds, sunflower seeds, almonds, raisins, chia seeds and peanuts. Balanced goodness for a hearty, energy boosting meal.", 
+    price: "180", 
+    category: "allday" },
+
+    { id: 19, img: menuImg19, name: "Coconut Milk Oats with Nuts and Chocolate", 
+    description: "The richness of coconut milk in wholesome blend of oats, flax seeds, pumpkin seeds, sunflower seeds, almonds, raisins, chia seeds, and chocolate.", 
+    price: "210", 
+    category: "allday" },
+
+    { id: 20, img: menuImg20, name: "Paneer Rise n Rice Combo", 
+    description: "Creamy paneer cooked in thick coconut milk gravy, fluffy steamed rice, and a colorful medley of sauted veggies.", 
+    price: "285", 
+    category: "meals" },
+
+    { id: 21, img: menuImg21, name: "Rise n Rice meal", 
+    description: "Tender chicken cooked in coconut milk garvy,  perfectly boiled egg [1 piece], fluffy steamed rice and a colorful medley of sautéed veggies.", 
+    price: "290", 
+    category: "meals" },
+
+    { id: 22, img: menuImg22, name: "Boneless Chicken Quinoa Curry with Steamed Rice", 
+    description: "Chicken quinoa curry paired with steamed rice and sautéed veggies wholesome and flavorful.", 
+    price: "295", 
+    category: "meals" },
+
+    { id: 23, img: menuImg23, name: "Paneer Quinoa Curry with Steamed Rice", 
+    description: "Creamy paneer curry paired with steamed rice and sautéed veggies wholesome and flavourful.", 
+    price: "275", 
+    category: "meals" },
+    
+    { id: 24, img: menuImg24, name: "Healthy Peanut and Banana Protein Smoothie", 
+    description: "The classic puree of banana, protein powder and peanut butter.", 
+    price: "180", 
+    category: "drinks" },
+
+    { id: 25, img: menuImg25, name: "Muskmelon Honey Smoothie", 
+    description: "Refreshing muskmelon smoothie with the sweetness of honey making you full yet light.", 
+    price: "190", 
+    category: "drinks" },
+
+    { id: 26, img: menuImg26, name: "Watermelon and Banana Protein Smoothie", 
+    description: "Thick and smooth banana smoothie with the freshness of watermelon giving you the natural energy boost for the day. The antioxidative and anti inflammatory properties of peanut butter with vanilla protein powder keeps you dip and full.", 
+    price: "180", 
+    category: "drinks" },
+
+    { id: 27, img: menuImg27, name: "Papaya Fat Burner Smoothie", 
+    description: "Smoothie full of papain enzyme breaks down the complexes of proteins for building muscles.", 
+    price: "180", 
+    category: "drinks" },
+
+    { id: 28, img: menuImg28, name: "Apple Beetroot Carrot Juice", 
+    description: "", 
+    price: "120", 
+    category: "drinks" },
+
+    { id: 29, img: menuImg29, name: "Cucumber Ginger Chia Fresca", 
+    description: "Refreshing beverage on a warm day or a great post workout treat with an interesting twist of chia seeds.", 
+    price: "80", 
+    category: "drinks" },
+
+    { id: 30, img: menuImg30, name: "Lime Juice with Honey & Chia Seeds", 
+    description: "The thirst quenching lime beverage with a small hint of sweetness seasoned with chia seeds.", 
+    price: "70", 
+    category: "drinks" },
+
+    { id: 31, img: menuImg31, name: "Watermelon Juice", 
+    description: "", 
+    price: "90", 
+    category: "drinks" },
+
+    { id: 32, img: menuImg32, name: "Pineapple Juice", 
+    description: "", 
+    price: "90", 
+    category: "drinks" },
+
+    { id: 33, img: menuImg33, name: "Grape Juice", 
+    description: "", 
+    price: "90", 
+    category: "drinks" },
+
+    { id: 34, img: menuImg34, name: "Mosambi Juice", 
+    description: "Enjoy the yummy refreshing drink to beat the heat.", 
+    price: "90", 
+    category: "drinks" },
+
+    { id: 35, img: menuImg35, name: "Pomegranate Juice", 
+    description: "Enjoy this antioxidant boost of a drink and best the heat.", 
+    price: "130", 
+    category: "drinks" },
+
+    { id: 36, img: menuImg36, name: "Tender Coconut Shake", 
+    description: "A delicious drink made with fresh coconut, blended to perfection. No added sweeteners.", 
+    price: "180", 
+    category: "drinks" },
+
+    { id: 37, img: menuImg37, name: "Raagi Hot Chocolate", 
+    description: "Indulge in this yummy hot chocolate which lets you satisfy all your sugar cravings without making you feel guilty.", 
+    price: "200", 
+    category: "desserts" },
+
+    { id: 38, img: menuImg38, name: "Fruit Bowl with Nuts", 
+    description: "Fresh fruit bowl of apple, pomegranate, papaya, banana, guava muskmelon and watermelon with trail mix of raisins, almonds, pumpkin seed, chia, flax, sunflower for healthy snacking.", 
+    price: "180", 
+    category: "desserts" },
+
+    { id: 39, img: menuImg39, name: "Honey Drizzled Fruit Bowl", 
+    description: "Fruit bowl of orange, grapes, muskmelon, banana, pineapple, guava and watermelon drizzled with honey and nuts.", 
+    price: "175", 
+    category: "desserts" },
+
+    
   
+]
+
+export const fourthScroll = [
+    { id: 1, icon: "Abcd", title: "Organically Sourced Ingredients", content:"Only high quality Romaine lettuce used, bringing higher levels of Vitamins A, K and Folate to your salad while maintaining a crispy crunch."},
+    {id: 2, icon: "efgh", title: "Fresh and Flavorful", content:" Each bowl is freshly prepared for your order with the best ingredients. Choose your flavors with endless customizations."},
+    {id:3, icon: "fgh", title: "NoFat Content", content: "Avoid unnecessary calories and unhealthy fats, and meet your goals of a balanced and nutritious diet"}
+
 ]
