0d075cefea56cfcec099ca359bb849863df68f2b
Manu-Madhu <manum66466@gmail.com>
2024-06-22 22:16:07 +0530

    feature-impliment the emailjs on contact page

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,9 +1,88 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { toast } from "react-toastify";
+import { Comment } from "react-loader-spinner";
 
 const ContactUs = () => {
-  useEffect(()=>{
-    window.scrollTo(0,0)
-   },[])
+  const ServiceId = "service_rt8txz3";
+  const TemplateId = "template_hr26a6c";
+  const PublicKey = "JIrSQu9e8Afu-oG8U";
+
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const validateForm = () => {
+    let haveError = false;
+
+    if (!formData.name?.trim()) {
+      haveError = true;
+      toast.error("Name is required");
+    }
+
+    if (!/^\S+@\S+\.\S+$/.test(formData.email)) {
+      haveError = true;
+      toast.error("Please enter valid email");
+    }
+
+    if (!formData.message?.trim()) {
+      haveError = true;
+      toast.error("Message is required");
+    }
+
+    return haveError;
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (validateForm()) return;
+
+    setLoading(true);
+    emailjs
+      .sendForm(ServiceId, TemplateId, e.target, PublicKey)
+      .then((result) => {
+        console.log("Email sent successfully!");
+
+        toast.success("Message sent successfully");
+
+        setFormData({
+          name: "",
+          email: "",
+          phone: "",
+          message: "",
+        });
+
+        setLoading(false);
+
+        console.log("Form submitted:", result);
+      })
+      .catch((error) => {
+        console.error("Error sending email:", error);
+        toast.error("Something went wrong");
+        // setErrors((prev) => ({ ...prev, server: "Something went wrong" }));
+        setLoading(false);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className="min-h-[75vh] mb-20 md:mb-40">
       <div className="bg-[#F9DB79] h-[30px] hidden md:flex"></div>
@@ -24,7 +103,8 @@ const ContactUs = () => {
 
         {/* ------------COntact Form ------------ */}
         <form
-          action=""
+          onSubmit={handleSubmit}
+          action="submit"
           className="max-w- flex flex-col items-center justify-center mt-10 md:mt-20"
         >
           {/* ----- Form data */}
@@ -35,6 +115,9 @@ const ContactUs = () => {
               </label>
               <input
                 type="text"
+                name="name"
+                onChange={handleChange}
+                value={formData?.name}
                 required
                 className="border border-black p-2 focus:outline-none"
                 placeholder="Name"
@@ -46,6 +129,9 @@ const ContactUs = () => {
               </label>
               <input
                 type="email"
+                name="email"
+                onChange={handleChange}
+                value={formData?.email}
                 required
                 className="border border-black p-2 focus:outline-none"
                 placeholder="Email"
@@ -57,6 +143,9 @@ const ContactUs = () => {
               </label>
               <input
                 type="number"
+                name="phone"
+                onChange={handleChange}
+                value={formData?.phone}
                 required
                 className="border border-black p-2 focus:outline-none"
                 placeholder="Number"
@@ -73,6 +162,9 @@ const ContactUs = () => {
               </label>
               <textarea
                 type="text"
+                name="message"
+                onChange={handleChange}
+                value={formData?.message}
                 required
                 className="border border-black p-2 h-[20vh]"
                 placeholder="Leave a Message ........."
@@ -80,12 +172,27 @@ const ContactUs = () => {
             </div>
           </div>
 
-
           <div className="w-full mt-10 flex items-center justify-center">
-            <button className="bg-black text-white p-3 px-5 font-primaryFont tracking-wider text-xl">Send Now</button>
+            <button className="bg-black text-white p-3 px-5 font-primaryFont tracking-wider text-xl">
+              Send Now
+            </button>
           </div>
         </form>
       </div>
+      {loading && (
+        <div className="w-screen h-screen flex items-center justify-center fixed top-0 right-0 bg-black/70">
+          <Comment
+            visible={true}
+            height="200"
+            width="200"
+            ariaLabel="comment-loading"
+            wrapperStyle={{}}
+            wrapperClass="comment-wrapper"
+            color="#fff"
+            backgroundColor="#F9DB79"
+          />
+        </div>
+      )}
     </div>
   );
 };
